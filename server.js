3085cd96e36b442d3f6d9c1768d4fdf9a76c6bc6
Abdoulaye-44 <baabou073@gmail.com>
2026-05-22 21:38:55 +0000

    cloudiary

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Configuration Cloudinary
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 // Stockage images sur Cloudinary
